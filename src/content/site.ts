b39a6698399823b3779d056bee947146f537d793
Prince Patel <prince13142004@gmail.com>
2026-08-02 12:02:01 +0530

    content: add Street Bites menu and site info data, remove CRAV menu data

---
+++ src/content/site.ts
@@ -0,0 +1,32 @@
+export const siteInfo = {
+  name: "Street Bites",
+  legalName: "Streetbites Food Limited",
+  companyNumber: "17131409",
+  tagline: "Taste of Street Culture",
+  address: {
+    line1: "1A North Street",
+    city: "Wolverhampton",
+    postcode: "WV1 1RE",
+    full: "1A North Street, Wolverhampton, WV1 1RE",
+  },
+  phone: "+447438017394",
+  phoneDisplay: "+44 7438 017394",
+  email: "streetbites.wv1@gmail.com",
+  social: {
+    instagram: "https://instagram.com/Streetbites",
+    facebook: "https://facebook.com/Streetbites",
+  },
+  delivery: {
+    // Search-results links until the restaurant's exact store page on each
+    // platform is confirmed; swap for the direct profile URL when known.
+    justEat: "https://www.just-eat.co.uk/search?q=Street%20Bites%20Wolverhampton",
+    uberEats: "https://www.ubereats.com/gb/search?q=Street%20Bites%20Wolverhampton",
+  },
+  hours: [
+    { day: "Monday – Thursday", time: "12:00 PM – 10:00 PM" },
+    { day: "Friday – Saturday", time: "12:00 PM – 11:00 PM" },
+    { day: "Sunday", time: "1:00 PM – 9:00 PM" },
+  ],
+  mapEmbedSrc:
+    "https://www.google.com/maps?q=1A+North+Street,+Wolverhampton,+WV1+1RE&output=embed",
+};
